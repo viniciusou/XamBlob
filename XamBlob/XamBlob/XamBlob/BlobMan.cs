fa6c8a514aabdab79916bf589f82c5989b41f56f
MADERO\vinicius.uemura <vinicius.uemura@restaurantemadero.com.br>
2017-11-01 17:14:51 -0200

    Initial commit

---
+++ XamBlob/XamBlob/XamBlob/BlobMan.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+namespace XamBlob
+{
+    //Blob Manager class
+    public class BlobMan
+    {
+        public static BlobMan Instance { get; } = new BlobMan();
+
+        private BlobMan()
+        {
+            _imagesContainer = _blobClient.GetContainerReference("images");
+        }
+
+        // Get this from the Azure Portal by clicking the "key" icon of the storage.
+        const string connectionString = "azure_blob_storage_key_goes_here";
+
+        // Create the blob client.
+        CloudBlobClient _blobClient = CloudStorageAccount
+            .Parse(connectionString)
+            .CreateCloudBlobClient();
+
+
+        CloudBlobContainer _imagesContainer;
+
+        public async Task<List<Uri>> GetAllBlobUrisAsync()
+        {
+            // Not quite perfect: requires multiple queries if there are many blobs.
+            var contToken = new BlobContinuationToken();
+            var allBlobs = await _imagesContainer.ListBlobsSegmentedAsync(contToken).ConfigureAwait(false);
+            
+            var uris = allBlobs.Results.Select(b => b.Uri).ToList();
+
+            return uris;
+        }
+
+        public async Task UploadFileAsync(string localPath)
+        {
+            string uniqueBlobName = Guid.NewGuid().ToString();
+            uniqueBlobName += Path.GetExtension(localPath);
+            var blobRef = _imagesContainer.GetBlockBlobReference(uniqueBlobName);
+
+            // Can upload files, streams, text, ...
+            await blobRef.UploadFromFileAsync(localPath).ConfigureAwait(false);
+        }
+    }
+}
---
+++ XamBlob/XamBlob/XamBlob/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamBlob
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            rootLayout.Children.Insert(0, new ScrollView
+            {
+                Content = _wrapLayout,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            });
+
+        }
+
+        void ShowStatus(string msg, bool active = false)
+        {
+            txtStatus.Text = msg;
+            activity.IsVisible = activity.IsRunning = active;
+        }
+
+        WrapLayout _wrapLayout = new WrapLayout();
+        bool _isRefreshing;
+
+        async void HandleAddClicked(object sender, System.EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            Console.WriteLine("File: " + file.Path);
+
+            ShowStatus("Uploading...", true);
+            await BlobMan.Instance.UploadFileAsync(file.Path);
+            ShowStatus("Upload done.");
+        }
+
+        async void HandleRefreshClicked(object sender, System.EventArgs e)
+        {
+            if (_isRefreshing)
+            {
+                return;
+            }
+
+            _isRefreshing = true;
+
+            ShowStatus("Refreshing...", true);
+
+            try
+            {
+                await UpdateAllImagesAsync();
+            }
+            finally
+            {
+                _isRefreshing = false;
+                ShowStatus("Done refreshing");
+            }
+        }
+
+        async Task UpdateAllImagesAsync()
+        {
+            _wrapLayout.Children.Clear();
+            var uris = await BlobMan.Instance.GetAllBlobUrisAsync();
+            foreach (var uri in uris)
+            {
+                var img = new Image
+                {
+                    Source = ImageSource.FromUri(uri),
+                    WidthRequest = 70
+                };
+
+                _wrapLayout.Children.Add(img);
+            }
+        }
+    }
+}
